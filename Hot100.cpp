fd2ae90c3d1c039fab1cddcb8b72b705ceebdd92
ljr <3179423949@qq.com>
2025-02-02 12:19:59 +0800

    T416提交

--- Hot100.cpp
+++ Hot100.cpp
@@ -121,3 +121,34 @@ public:
     }
 };
 */
+
+
+
+/*
+// T416 分割等和子集
+// 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % 2) return false; // 和不为 2 的倍数
+        int target = total / 2; // 目标
+
+        // 背包问题
+        vector<vector<int>> dp(nums.size(), vector<int>(target + 1)); // 初始化，dp[i][j] 表示前i个数字是否存在某些和为j
+        dp[0][0] = true; 
+        if(nums[0] <= target) dp[0][nums[0]] = true; // 初始化
+        for(int i = 1; i < nums.size(); i++)
+        {
+            for(int j = 0; j <= target; j++)
+            {
+                if (j >= nums[i]) dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]; // 不选i和选i
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[nums.size() - 1][target];
+    }
+};
+*/
