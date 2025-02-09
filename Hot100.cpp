0395533378e42808e46f66c9f06443c492939d33
ljr <3179423949@qq.com>
2025-02-09 21:02:54 +0800

    2025.2.9 -- 2

--- Hot100.cpp
+++ Hot100.cpp
@@ -533,3 +533,43 @@ int main() {
     return 0;
 }
 */
+
+
+/*
+// T437 路径总和
+// 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+// 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+class Solution {
+public:
+    long long res = 0;
+    void pathSum_1(TreeNode* root, long long targetSum, bool isFetch) {
+        if(root->val == targetSum) res++;
+        if(isFetch) // 已经选取过
+        {
+            
+            long long tmp = targetSum - root->val; // 防止溢出
+            if(root->right) pathSum_1(root->right, tmp, true); // 若已经选过，那么必须选，否则就不是连续的
+            if(root->left) pathSum_1(root->left, tmp, true); // 若已经选过，那么必须选，否则就不是连续的
+        }
+        else  // 未选取过
+        {
+            if(root->right)
+            {
+                pathSum_1(root->right, targetSum, false); // 不选
+                pathSum_1(root->right, targetSum - root->val, true); // 选
+            }
+            if(root->left)
+            {
+                pathSum_1(root->left, targetSum, false);
+                pathSum_1(root->left, targetSum - root->val, true);
+            }
+        }
+    }
+
+    long long pathSum(TreeNode* root, int targetSum) {
+        if(!root) return 0;
+        pathSum_1(root, targetSum, false);
+        return res;
+    }
+};
+*/
