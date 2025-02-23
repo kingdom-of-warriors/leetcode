34eadbc7d21b779302f9ebde75cb9839a84082de
ljr <3179423949@qq.com>
2025-02-23 23:45:28 +0800

    2025.2.23 -- 2

--- Hot100.cpp
+++ Hot100.cpp
@@ -709,3 +709,47 @@ public:
     }
 };
 */
+
+
+/*
+// T141 环形链表1\2
+// 给你一个链表的头节点 head ，判断链表中是否有环。
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+
+// 环形链表1：如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+// 环形链表2：返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+// 思路：快慢指针，方法2：哈希表记录第一个重复的节点
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode *fast = head, *slow = head; // 快慢指针
+        while(true)
+        {
+            if(!fast->next || !slow->next) return false; // 只要不能继续，一定是没有环的
+            if(!fast->next->next) return false; // 只要不能继续，一定是没有环的
+            fast = fast->next->next;
+            slow = slow->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return nullptr;
+        unordered_map<ListNode*, int> hash_map; // 用来记录第一个重复的点
+        ListNode *p = head;  // 用于遍历
+        while(p) // 修改：使用p作为循环条件而不是true
+        {
+            if(hash_map[p] == 1) // 修改：检查是否已经访问过
+            {
+                return p;
+            }
+            hash_map[p]++;
+            p = p->next;
+        }
+        return nullptr;
+    }
+};
+*/
