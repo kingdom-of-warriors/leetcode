acc26a70c3d3d348d9eb055b8c542fff3c49416f
ljr <3179423949@qq.com>
2025-03-28 10:45:21 +0800

    2025.3.28

--- Hot100.cpp
+++ Hot100.cpp
@@ -1247,12 +1247,92 @@ public:
 //     }
 // };
 
-int main()
-{
-    cout << "请输入n: ";
-    int n; cin >> n;
-    vector<int> F(n + 1); // F(n) 为得到n的最小操作数
-    for(int i = 1; i <= n; i++) F[i] = ((i % 2) ? F[i - 1] : min(F[i / 2], F[i - 1])) + 1;
-    cout << "至少需要的操作数为："<< F[n] << endl;
-    return 0;
-}
+// int main()
+// {
+//     cout << "请输入n: ";
+//     int n; cin >> n;
+//     vector<int> F(n + 1); // F(n) 为得到n的最小操作数
+//     for(int i = 1; i <= n; i++) F[i] = ((i % 2) ? F[i - 1] : min(F[i / 2], F[i - 1])) + 1;
+//     cout << "至少需要的操作数为："<< F[n] << endl;
+//     return 0;
+// }
+
+
+
+// 二分查找
+
+
+/*
+// T35 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+// 请必须使用时间复杂度为 O(log n) 的算法。
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return searchInsertInVector(nums, target, 0, nums.size() - 1);
+    }
+
+    // 表示在[begin, end]之间应该插入哪里，满足numsbegin<= target <= numsend
+    int searchInsertInVector(vector<int>& nums, int target, int begin, int end) 
+    {
+        if(begin == end)
+        {
+            if(target <= nums[begin]) return begin;
+            else return begin + 1;
+        }
+        if(begin + 1 == end)
+        {
+            if(target <= nums[begin]) return begin;
+            if(target > nums[end]) return end + 1;
+            else return end;
+        }
+        int mid = (end - begin) / 2 + begin;
+        if(nums[mid] == target) return mid;
+        if(nums[mid] < target) return searchInsertInVector(nums, target, mid, end);
+        if(nums[mid] > target) return searchInsertInVector(nums, target, begin, mid);
+        return 0;
+    }
+
+    // 下面是标准的二分查找代码
+    int searchInsert_1(vector<int>& nums, int target)
+    {
+        int left = 0, right = nums.size() - 1;
+        while(left <= right)
+        {
+            int mid = (left - right) / 2 + right;
+            if(target == nums[mid]) return mid;
+            else if(target > nums[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+};
+*/
+
+
+
+/*
+// T74 搜索二维矩阵
+// 给你一个满足下述两条属性的 m x n 整数矩阵：
+// 每行中的整数从左到右按非严格递增顺序排列。
+// 每行的第一个整数大于前一行的最后一个整数。
+// 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+
+// 思路：两种方法：当作一维数组来找复杂度为log(MN)，二是先找行再找列，复杂度也为log(M)+log(N)=log(MN)
+class Solution {
+public:
+    int row; int col;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        row = matrix.size(); col = matrix[0].size();
+        int left = 0, right = row * col - 1;
+        while(left <= right)
+        {
+            int mid = (right - left) / 2 + left;
+            if(target == matrix[mid / col][mid % col]) return true;
+            else if(target < matrix[mid / col][mid % col]) right = mid - 1;
+            else left = mid + 1;
+        }
+        return false;
+    }
+};
+*/
